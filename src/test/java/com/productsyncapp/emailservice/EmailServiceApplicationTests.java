c2a77c936c498d196b6eba302bdeaaf3fc1833b3
rijogeorge <rijothedeveloper@gmail.com>
2024-03-18 00:33:23 -0700

    first commit

---
+++ src/test/java/com/productsyncapp/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productsyncapp.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
