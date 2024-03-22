73ee397e9c0e84ac97c701ff5a512dd4de0ecf12
rijogeorge <rijothedeveloper@gmail.com>
2024-03-21 18:26:36 -0700

    path change

--- src/main/java/com/productsyncapp/emailservice/controller/EmailController.java
+++ src/main/java/com/productsyncapp/emailservice/controller/EmailController.java
@@ -3,14 +3,18 @@ package com.productsyncapp.emailservice.controller;
 import com.productsyncapp.emailservice.dto.RegisterationEmailRequest;
 import com.productsyncapp.emailservice.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/emailservice")
 public class EmailController {
     @Autowired
     private EmailService emailService;
+
+    @GetMapping("/status")
+    public String getStatus(){
+        return "Working";
+    }
     @PostMapping("/sendMail")
     public String sendMail(@RequestBody RegisterationEmailRequest registerationEmailRequest)
     {
